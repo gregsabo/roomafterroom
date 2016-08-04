752cb8c9b393a507cdbd6a2cc9e8bb59cc2ed419
Greg Sabo <gregsabo@gmail.com>
2016-08-03 21:49:26 -0700

    Some whys

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
+'use strict';
+var $ = window.jQuery;
+var _ = window._;
+
+function choose(inArray) {
+  return inArray[Math.floor(Math.random() * inArray.length)];
+}
+
+function shiftOne() {
+  $(choose($('.why'))).css({
+    'margin-left': Math.random() * 5,
+    'margin-right': Math.random() * 5
+  });
+}
+
+var lastHide;
+function showHandOnMove() {
+  $('body').on('mousemove', function() {
+    if (lastHide) {
+      clearTimeout(lastHide);
+    }
+    $('.hand').addClass('showing');
+    lastHide = setTimeout(function() {
+      $('.hand').removeClass('showing');
+    }, 1000);
+  });
+
+}
+
 function main() {
   var $why = $('.why');
-  _.range(100, function() {
-    $('body').append($why.clone());
-  }
-};
+  _.range(150).forEach(function() {
+    $why.clone().appendTo($('body'));
+  });
+  showHandOnMove();
+  setInterval(shiftOne, 10);
+}
+
 
 $(main);
